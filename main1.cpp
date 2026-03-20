119b55c2dbd43c7a5cb2ed6c483d86c83856e22b
VikaSav13012007 <savchenkoviktoria2007@gmail.com>
2026-03-21 02:20:28 +0700

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,58 +1,211 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
+enum VehicleType { Car, Truck, Motorcycle };
 
-struct Student {
-    int number_record_book;
-    string name;
-    enum Status { Excellent, Good, Satisfactory, Unsatisfactory };
-    Status student_status; 
+struct Engine {
+    int power;
+    double volume;
 };
 
-Student::Status get_status() {
-    string status="";
-    
-    while(true){
-        cout << "введите статус успеваемости (Excellent, Good, Satisfactory, Unsatisfactory): ";
-        cin >> status;
-        if (status == "Excellent") return Student::Excellent;
-        if (status == "Good") return Student::Good;
-        if (status == "Satisfactory") return Student::Satisfactory;
-        if (status == "Unsatisfactory") return Student::Unsatisfactory;
-        
-    }
-    
-}
+struct Vehicle {
+    VehicleType type;
+    string model;
+    Engine engine;
+};
 
-int get_number_record_book() {
+struct Vehicle_without_nesting_structures {
+    string type;
+    string model;
+    int power;
+    double volume;
+};
+
+int get_number() {
     int number;
     while (!(cin >> number)) {
         cout << "введено не число. введите еще раз: ";
         cin.clear(); 
         cin.ignore(10000, '\n'); 
     }
-    
+    cin.ignore(10000, '\n');
     return number;
 }
 
-int main() {
-    string all_status[4]={"Excellent", "Good", "Satisfactory", "Unsatisfactory"};
-    Student person;
-    cout << "введите имя: ";
-    cin >> person.name;
-    cout << "введите номер зачетной книжки: ";
-    person.number_record_book=get_number_record_book();
-    person.student_status = get_status();
-    cout << "name: " << person.name << endl;
-    cout << "number record book: " << person.number_record_book << endl;
-    cout << "status: " << all_status[person.student_status] << endl;
-    cout << "сравнение размера памяти:"<< endl;
-    size_t size_struct = sizeof(person);
-    cout << "размер структуры Student: " << size_struct << " байт" << endl;
-    int test_arr_int[4];
-    size_t size_array = sizeof(test_arr_int);
-    cout << "размер массива из 4 int: " << size_array << " байт" << endl;
+string type_to_string(VehicleType type) {  
+    if (type == Car) return "Car";
+    if (type == Truck) return "Truck";
+    if (type == Motorcycle) return "Motorcycle";
+}
 
+void input_vehicles(Vehicle vehicles[]) {
+    for (int i = 0; i < 5; i++) {
+        int type_input;
+        while (true) {
+            cout << "введите данные для транспорта " << i + 1 << endl;
+            cout << "тип (0-Car, 1-Truck, 2-Motorcycle): ";
+            
+            if (cin >> type_input) {
+                if (type_input >= 0 && type_input <= 2) {
+                    break;
+                } else {
+                    cout << "неправильно. введите 0, 1 или 2" << endl;
+                }
+            } 
+            else {
+                cout << "неправильно. введите число 0, 1 или 2" << endl;
+                cin.clear();
+                cin.ignore(10000, '\n');
+            }
+        }
+        
+        vehicles[i].type = (VehicleType)type_input;
+        
+        cout << "модель: ";
+        cin >> vehicles[i].model;
+        
+        cout << "мощность двигателя: ";
+        vehicles[i].engine.power = get_number();
+        
+        cout << "объем двигателя: ";
+        vehicles[i].engine.volume = get_number();
+    }
+}
+
+void print_vehicles(Vehicle vehicles[]) {
+    cout << "список транспорта:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << i + 1 << ". " << type_to_string(vehicles[i].type) << " " << vehicles[i].model
+             << ", мощность: " << vehicles[i].engine.power
+             << ", объем: " << vehicles[i].engine.volume << " л" << endl;
+    }
+}
+
+void sort_by_power(Vehicle vehicles[]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4 - i; j++) {
+            if (vehicles[j].engine.power > vehicles[j + 1].engine.power) {
+                Vehicle temp = vehicles[j];
+                vehicles[j] = vehicles[j + 1];
+                vehicles[j + 1] = temp;
+            }
+        }
+    }
+    cout << "транспорт отсортирован по мощности" << endl;
+}
+
+void print_by_type(Vehicle vehicles[]) {
+    bool flag_found = false;
+    int type; 
+    while (true) {
+        cout << "введите тип транспорта (0-Car, 1-Truck, 2-Motorcycle): ";
+        type = get_number();
+        if (type >= 0 && type <= 2) {
+            break;
+        }
+        cout << "неправильно" << endl;
+    }
+    
+    cout << "транспорт типа " << type_to_string((VehicleType)type) << ":" << endl;
+    for (int i = 0; i < 5; i++) {
+        if (vehicles[i].type == (VehicleType)type) {
+            cout << vehicles[i].model << ", мощность: " << vehicles[i].engine.power
+                 << ", объем: " << vehicles[i].engine.volume << " л" << endl;
+            flag_found = true;
+        }
+    }
+    if (!flag_found) {
+        cout << "нет транспорта этого типа" << endl;
+    }
+}
+
+void input_simple_vehicles(Vehicle_without_nesting_structures simple_vehicles[], int size) {
+    string type_names[3] = {"Car", "Truck", "Motorcycle"};
+    for (int i = 0; i < size; i++) {
+        simple_vehicles[i].type = type_names[i % 3];
+        simple_vehicles[i].model = "Model" + to_string(i + 1);
+        simple_vehicles[i].power = rand() % 600;
+        simple_vehicles[i].volume = (rand() % 300) / 10.0;
+    }
+}
+
+void sort_simple_vehicles(Vehicle_without_nesting_structures simple_vehicles[], int size) {
+    Vehicle_without_nesting_structures temp;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (simple_vehicles[j].power > simple_vehicles[j + 1].power) {
+                temp = simple_vehicles[j];
+                simple_vehicles[j] = simple_vehicles[j + 1];
+                simple_vehicles[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void print_simple_vehicles(Vehicle_without_nesting_structures simple_vehicles[], int size) {
+    cout << "список транспорта:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << i + 1 << ". " << simple_vehicles[i].type << " " << simple_vehicles[i].model
+             << ", мощность: " << simple_vehicles[i].power
+             << ", объем: " << simple_vehicles[i].volume << " л" << endl;
+    }
+}
+
+void sort_vehicles_rand(Vehicle vehicles_rand[], int size) {
+    Vehicle temp;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (vehicles_rand[j].engine.power > vehicles_rand[j + 1].engine.power) {
+                temp = vehicles_rand[j];
+                vehicles_rand[j] = vehicles_rand[j + 1];
+                vehicles_rand[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+void input_vehicles_random(Vehicle vehicles_rand[], int size) {
+    for (int i = 0; i < size; i++) {
+        vehicles_rand[i].model = "Model" + to_string(i + 1);
+        vehicles_rand[i].engine.power = rand() % 600;
+        vehicles_rand[i].engine.volume = (rand() % 300) / 10.0;
+    }
+}
+int main() {
+    Vehicle vehicles[5];
+    input_vehicles(vehicles);
+    sort_by_power(vehicles);
+    print_vehicles(vehicles);
+    
+    cout << "поиск по типу:" << endl;
+    print_by_type(vehicles);
+
+    const int SIZE = 100;
+    Vehicle vehicles_rand[SIZE];
+    input_vehicles_random(vehicles_rand,SIZE);
+    clock_t start1 = clock();
+    sort_vehicles_rand(vehicles_rand,SIZE);
+    clock_t end1 = clock();
+    double time1 = double(end1 - start1) / CLOCKS_PER_SEC;
+    cout << "время сортировки со структурой: " << time1 << " сек" << endl;
+
+
+    Vehicle_without_nesting_structures simple_vehicles[SIZE];
+    input_simple_vehicles(simple_vehicles, SIZE);
+    
+    clock_t start2 = clock();
+    sort_simple_vehicles(simple_vehicles, SIZE);
+    clock_t end2 = clock();
+   
+    
+    double time2 = double(end2 - start2) / CLOCKS_PER_SEC;
+    cout << "время сортировки без структуры: " << time2 << " сек" << endl;
+    
+    print_simple_vehicles(simple_vehicles, SIZE);
+    
     return 0;
 }
+
